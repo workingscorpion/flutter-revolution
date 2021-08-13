bd07d2abefa8ca07364aa7e0afae73f60ee63aed
workingscorpion <workingscorpion@globalorder.co.kr>
2021-08-13 19:45:13 +0900

    feat: splash

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,3 +1,4 @@
+import 'package:btrips/appRouter.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatefulWidget {
@@ -9,13 +10,59 @@ class SplashPage extends StatefulWidget {
 
 class _SplashPageState extends State<SplashPage> {
   @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration(milliseconds: 2000), () {
+      AppRouter.toHomePage();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('header'),
-      ),
-      body: Center(
-        child: Text('asdf'),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: EdgeInsets.only(bottom: 20),
+              child: Row(
+                children: [
+                  Container(
+                    height: 40,
+                    margin: EdgeInsets.only(right: 10),
+                    child: Image.asset(
+                      'assets/logo/logo.png',
+                    ),
+                  ),
+                  Text(
+                    'BTRIPS',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                  ),
+                ],
+              ),
+            ),
+            Text(
+              '암호화폐 정보를 한눈에',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black54,
+                fontSize: 20,
+              ),
+            ),
+            Text(
+              '비트립스',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black54,
+                fontSize: 20,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
