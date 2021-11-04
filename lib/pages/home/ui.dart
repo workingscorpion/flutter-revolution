eedb17930c4158996fbbe0a0e29341f84dbb436a
workingscorpion <workingscorpion@globalorder.co.kr>
2021-11-04 17:41:32 +0900

    refactor:

--- lib/pages/home/ui.dart
+++ lib/pages/home/ui.dart
@@ -48,6 +48,7 @@ class _UIPageState extends State<UIPage> {
                 // TODO: 토스트 자체 제작
                 // TODO: snackbar
                 // TODO: custom switch(from 유진쓰)
+                // TODO: 고정된 component 사이에 자동으로 margin 주는 ui(마지막은 margin 없게)
               ],
             ),
           ),
