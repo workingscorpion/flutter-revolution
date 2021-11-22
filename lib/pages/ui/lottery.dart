22d0e9d58a4414b1d0ba07c40398e6263b4aeb37
workingscorpion <workingscorpion@globalorder.co.kr>
2021-11-22 11:02:28 +0900

    fix:

--- lib/pages/ui/lottery.dart
+++ lib/pages/ui/lottery.dart
@@ -18,25 +18,26 @@ class LotteryPage extends StatelessWidget {
           padding: EdgeInsets.symmetric(horizontal: 25, vertical: 40),
           children: [
             CustomBox(
-              title: 'library Lottery - scratcher',
-              child: SingleChildScrollView(
-                child: Scratcher(
-                  threshold: 50,
-                  brushSize: 30,
-                  color: Colors.grey,
-                  // image: Image.asset('assets/home/roulette.png'),
-                  // onChange: (double value) async {
-                  //   if (value > 50.0) {
-                  //     // await showAlert();
-                  //   }
-                  // },
-                  onThreshold: () {
-                    print('Congraturation');
-                  },
-                  child: Container(
-                    width: 200,
-                    height: 150,
-                    child: Image.asset('assets/home/lottery.png'),
+                title: 'library Lottery - scratcher',
+                child: SingleChildScrollView(
+                  child: Scratcher(
+                    threshold: 50,
+                    brushSize: 30,
+                    color: Colors.grey,
+                    // image: Image.asset('assets/home/roulette.png'),
+                    // onChange: (double value) async {
+                    //   if (value > 50.0) {
+                    //     // await showAlert();
+                    //   }
+                    // },
+                    onThreshold: () {
+                      print('Congraturation');
+                    },
+                    child: Container(
+                      width: 200,
+                      height: 150,
+                      child: Image.asset('assets/home/lottery.png'),
+                    ),
                   ),
                 )),
             // CustomBox(
