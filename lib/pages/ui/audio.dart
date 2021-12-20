4d5b6031823346885bca57cce444bce7516485c2
scorpion <workingscorpion@globalorder.co.kr>
2021-12-20 20:48:14 +0900

    feat: audio(ing)

--- lib/pages/ui/audio.dart
+++ lib/pages/ui/audio.dart
@@ -1,3 +1,5 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 class AudioPage extends StatelessWidget {
@@ -5,11 +7,36 @@ class AudioPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // AudioCache audioPlayer = AudioCache();
+    AudioCache audioPlayer = AudioCache();
+    // AudioCache audioPlayer = AudioCache(prefix: '/components/');
+
+    // playLocal() async {
+    //   int result =
+    //       await audioPlayer.play('/assets/components/audio.mp3', isLocal: true);
+    // }
+
     return Scaffold(
       body: SafeArea(
         child: ListView(
           padding: EdgeInsets.symmetric(horizontal: 25, vertical: 40),
-          children: [],
+          children: [
+            IconButton(
+              onPressed: () {
+                audioPlayer.play('audio.mp3');
+                // playLocal();
+              },
+              icon: Icon(Icons.play_arrow),
+            ),
+            IconButton(
+              onPressed: () {
+                audioPlayer.clearCache();
+                // audioPlayer. ('audio.mp3');
+                // playLocal();
+              },
+              icon: Icon(Icons.pause),
+            ),
+          ],
         ),
       ),
     );
