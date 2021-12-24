6b8cc189bc34083e1c50eab9ccaee6ddbec59d6c
workingscorpion <workingscorpion@globalorder.co.kr>
2021-12-24 14:04:09 +0900

    feat: blurpin

---
+++ lib/components/blurPin.dart
@@ -0,0 +1,217 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:revolution/appRouter.dart';
+import 'package:revolution/constants/customColors.dart';
+
+class BlurPin extends StatefulWidget {
+  BlurPin({Key key}) : super(key: key);
+
+  @override
+  _BlurPinState createState() => _BlurPinState();
+}
+
+class _BlurPinState extends State<BlurPin> {
+  List<int> initDigits = List<int>.generate(10, (i) => i)..shuffle();
+  String password = "";
+
+  int get length {
+    return password.length;
+  }
+
+  setPassword(int e) {
+    password += '$e';
+    setState(() {});
+    if (length >= 6) {
+      AppRouter.pop();
+    }
+  }
+
+  deletePassword() {
+    password = password.substring(0, password.length - 1);
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints(
+        minHeight: MediaQuery.of(context).size.height * .9,
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+        // clipBehavior: Clip.antiAliasWithSaveLayer,
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaY: 10, sigmaX: 10),
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                GestureDetector(
+                  onTap: () => AppRouter.pop(),
+                  child: Container(
+                    alignment: Alignment.centerLeft,
+                    child: Icon(
+                      Icons.clear,
+                      size: 25,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 60),
+                  child: Column(
+                    children: [
+                      title(),
+                      subtitle(),
+                    ],
+                  ),
+                ),
+                safeKeyPad(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  title() => Container(
+        margin: EdgeInsets.only(bottom: 10),
+        child: Text(
+          'Enter PIN',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+            color: Colors.white,
+          ),
+        ),
+      );
+
+  subtitle() => Text(
+        'Please Enter the 6 digit password.',
+        style: TextStyle(
+          fontSize: 13,
+          color: Colors.white,
+        ),
+      );
+
+  safeKeyPad() => Column(
+        children: [
+          digits(),
+          // Spacer(),
+          Container(
+            height: MediaQuery.of(context).size.height * .1,
+          ),
+          keyPads(),
+        ],
+      );
+
+  Widget digits() => Container(
+        margin: EdgeInsets.symmetric(
+          // horizontal: MediaQuery.of(context).size.width * .12,
+          vertical: 40,
+        ),
+        padding: EdgeInsets.symmetric(horizontal: 5),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(6, (i) => digit(i)),
+        ),
+      );
+
+  Widget keyPads() => Container(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: GridView.builder(
+          shrinkWrap: true,
+          physics: NeverScrollableScrollPhysics(),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            childAspectRatio: 6 / 4,
+          ),
+          itemCount: initDigits.length + 2,
+          itemBuilder: (BuildContext context, int index) => setKeyPad(index),
+        ),
+      );
+
+  Widget setKeyPad(int index) {
+    if (index == 10) {
+      return logoKeyPad();
+    }
+    if (index == 11) {
+      return password.length <= 0 ? emptyKeyPad() : keyPad(delete: true);
+    }
+
+    return keyPad(e: initDigits[index]);
+  }
+
+  Widget emptyKeyPad() => Container(
+        height: 50,
+        width: 25,
+        child: Center(),
+      );
+
+  Widget logoKeyPad() => Container(
+        height: 50,
+        width: 25,
+        child: Center(
+          child: Container(
+            height: 25,
+            child: Image.asset('assets/logo/logo.png'),
+          ),
+        ),
+      );
+
+  Widget keyPad({int e = 0, bool delete = false}) => GestureDetector(
+        onTap: () => !delete && e < 10 ? setPassword(e) : deletePassword(),
+        child: Container(
+          height: 25,
+          width: 25,
+          color: Colors.transparent,
+          child: Center(
+            child: delete
+                ? Icon(
+                    Icons.arrow_back_outlined,
+                    color: Colors.white,
+                  )
+                : e > 9
+                    ? Container()
+                    : Text(
+                        '$e',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontFamily: 'gmarket',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+          ),
+        ),
+      );
+
+  Widget digit(int index) => Builder(
+        builder: (BuildContext context) => index < length
+            ? circle(
+                color: Colors.white,
+                marginSize: index >= 6 ? 0 : 20,
+              )
+            : circle(
+                color: CustomColors.disabledGrey,
+                marginSize: index >= 5 ? 0 : 20,
+              ),
+      );
+
+  Widget circle({Color color, double marginSize}) => Container(
+        width: 20,
+        height: 20,
+        margin: EdgeInsets.only(right: marginSize),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: color,
+        ),
+      );
+}
--- lib/pages/ui/pin.dart
+++ lib/pages/ui/pin.dart
@@ -2,8 +2,10 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:revolution/appRouter.dart';
+import 'package:revolution/components/blurPin.dart';
 import 'package:revolution/components/customBox.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'package:revolution/constants/customColors.dart';
 
 class PinPage extends StatefulWidget {
   PinPage({Key key}) : super(key: key);
@@ -13,118 +15,26 @@ class PinPage extends StatefulWidget {
 }
 
 class _PinPageState extends State<PinPage> {
-  List<int> initDigits = List<int>.generate(10, (i) => i)..shuffle();
-  String password = "";
-
   showSlide() async {
     await showCupertinoModalBottomSheet(
       topRadius: Radius.circular(30),
-      backgroundColor: Colors.transparent,
+      backgroundColor: Colors.black.withOpacity(.45),
       context: context,
-      builder: (context) => BackdropFilter(
-        filter: ImageFilter.blur(sigmaY: 10, sigmaX: 10),
-        child: SingleChildScrollView(
-          child: Material(
-            color: Colors.transparent,
-            child: Container(
-              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
-              constraints: BoxConstraints(
-                minHeight: MediaQuery.of(context).size.height * .9,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  GestureDetector(
-                    onTap: () => AppRouter.pop(),
-                    child: Container(
-                      alignment: Alignment.centerLeft,
-                      child: Icon(
-                        Icons.clear,
-                        size: 25,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.symmetric(vertical: 60),
-                    child: Column(
-                      children: [
-                        title(),
-                        subtitle(),
-                      ],
-                    ),
-                  ),
-                  safeKeyPad(),
-                ],
-              ),
-            ),
-          ),
+      enableDrag: false,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
         ),
       ),
-      // Container(
-      //       constraints: BoxConstraints(
-      //         minHeight: MediaQuery.of(context).size.height * .9,
-      //       ),
-      //       child: Column(
-      //         crossAxisAlignment: CrossAxisAlignment.center,
-      //         children: [
-      //           Container(
-      //             padding: EdgeInsets.symmetric(horizontal: 50),
-      //             child: Column(
-      //               children: [
-      //                 title(),
-      //                 // subtitle(),
-      //               ],
-      //             ),
-      //           ),
-      //           // safeKeyPad(),
-      //         ],
-      //       ),
-      //     ),
-      // =============
-      // builder: (context) => SingleChildScrollView(
-      //   child: Material(
-      //     color: Colors.transparent,
-      //     child: Container(
-      //       constraints: BoxConstraints(
-      //         minHeight: MediaQuery.of(context).size.height * .9,
-      //       ),
-      //       child: Column(
-      //         crossAxisAlignment: CrossAxisAlignment.center,
-      //         children: [
-      //           Container(
-      //             padding: EdgeInsets.symmetric(horizontal: 50),
-      //             child: Column(
-      //               children: [
-      //                 title(),
-      //                 // subtitle(),
-      //               ],
-      //             ),
-      //           ),
-      //           // safeKeyPad(),
-      //         ],
-      //       ),
-      //     ),
-      //   ),
-      // ),
+      builder: (context) => SingleChildScrollView(
+        physics: NeverScrollableScrollPhysics(),
+        child: Material(
+          color: Colors.transparent,
+          child: BlurPin(),
+        ),
+      ),
     );
-  }
-
-  int get length {
-    return password.length;
-  }
-
-  setPassword(int e) {
-    password += '$e';
-    setState(() {});
-    if (length >= 6) {
-      AppRouter.pop();
-    }
-  }
-
-  deletePassword() {
-    password = password.substring(0, password.length - 1);
-    setState(() {});
   }
 
   @override
@@ -175,118 +85,4 @@ class _PinPageState extends State<PinPage> {
       ),
     );
   }
-
-  title() => Container(
-        margin: EdgeInsets.only(bottom: 10),
-        child: Text(
-          'Enter PIN',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 18,
-            color: Colors.white,
-          ),
-        ),
-      );
-
-  subtitle() => Text(
-        'Please Enter the 6 digit password.',
-        style: TextStyle(
-          fontSize: 13,
-          color: Colors.white,
-        ),
-      );
-
-  safeKeyPad() => Expanded(
-        child: Column(
-          children: [
-            digits(),
-            // Spacer(),
-            keyPads(),
-          ],
-        ),
-      );
-
-  Widget digits() => Container(
-        margin: EdgeInsets.symmetric(
-            horizontal: MediaQuery.of(context).size.width * .22, vertical: 40),
-        padding: EdgeInsets.symmetric(horizontal: 5),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: List.generate(6, (i) => digit(i)),
-        ),
-      );
-
-  Widget keyPads() => Container(
-        padding: EdgeInsets.symmetric(horizontal: 10),
-        child: GridView.builder(
-          shrinkWrap: true,
-          physics: NeverScrollableScrollPhysics(),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3,
-            childAspectRatio: 6 / 4,
-          ),
-          itemCount: initDigits.length + 2,
-          itemBuilder: (BuildContext context, int index) => setKeyPad(index),
-        ),
-      );
-
-  Widget setKeyPad(int index) {
-    if (index == 9) {
-      return emptyKeyPad();
-    }
-    if (index == 10) {
-      return keyPad(e: initDigits[index - 1]);
-    }
-    if (index == 11) {
-      return password.length <= 0 ? emptyKeyPad() : keyPad(delete: true);
-    }
-
-    return keyPad(e: initDigits[index]);
-  }
-
-  Widget emptyKeyPad() => Container(
-        height: 50,
-        width: 25,
-        child: Center(),
-      );
-
-  Widget keyPad({int e = 0, bool delete = false}) => GestureDetector(
-        onTap: () => !delete && e < 10 ? setPassword(e) : deletePassword(),
-        child: Container(
-          height: 25,
-          width: 25,
-          color: Colors.white,
-          child: Center(
-            child: delete
-                ? Icon(
-                    Icons.arrow_back_outlined,
-                    color: Colors.black,
-                  )
-                : e > 9
-                    ? Container()
-                    : Text(
-                        '$e',
-                        style: TextStyle(
-                          fontSize: 25,
-                          fontFamily: 'gmarket',
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-          ),
-        ),
-      );
-
-  Widget digit(int index) => Builder(
-        builder: (BuildContext context) =>
-            index < length ? circle(Colors.black) : circle(Colors.red),
-      );
-
-  Widget circle(Color color) => Container(
-        width: 20,
-        height: 20,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: color,
-        ),
-      );
 }
